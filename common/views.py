32cd4cc4413ba0b53195afb7a9060db893e615d8
russos <ro0ne@mail.ru>
2023-01-17 18:55:42 +0300

    update

---
+++ common/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from datetime import datetime
+
+from django.views import View
+from django.http import HttpResponse
+
+
+class CurrentDateView(View):
+   def get(self, request):
+       html = f"{datetime.now()}"
+       return HttpResponse(html)
